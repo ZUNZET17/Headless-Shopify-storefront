0601177a4dc7ba720586305ee08fec246f1de220
ZUNZET <ultimate.rebellion0@gmail.com>
2022-03-20 10:00:08 -0500

    Fix product quantity when changing product pages

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -8,7 +8,8 @@ export default function AddToCart(props) {
   let cartId;
   
   useEffect(() => {   
-    setVariantId(props.id)
+    setVariantId(props.id);
+    setQty(1);
   }, [props.id])
 
   function increment () {
